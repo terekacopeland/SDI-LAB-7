d2efcce9b6376cc57d7a1cc7a5f3062fd1168908
terekacopeland <starsxmexico@live.com>
2014-03-18 14:20:06 -0400

    calling the json data
    making sure json data appears

--- copeland_tereka_lab7.js
+++ copeland_tereka_lab7.js
@@ -1,1 +1,6 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+//SDI 1403
+//Lab 7 Working With Objects
+//Tereka Copeland
+
+console.log(jsonData);
--- copeland_tereka_lab7JSON.js
+++ copeland_tereka_lab7JSON.js
@@ -3,3 +3,38 @@
 //Working with objects
 //Tereka Copeland
 //JSON DATA
+
+var jsonData = {
+	
+	"musicFestivals":
+	[
+		
+		{
+			"festival": "Ultra",
+			"location": "Miami",
+			"yearAttended": 2011,
+			"genre": "dance music"
+		},
+		{
+			"festival": "Electric Daisy Carnival",
+			"location": "Las Vegas",
+			"yearAttended": 2012,
+			"genre": "dance music"
+		},
+		{
+			"festival": "TomorrowWorld",
+			"location": "Atlanta",
+			"yearAttended": 2013,
+			"genre": "dance music"
+		},
+		{
+			"festival": "Electric Daisy Carnival",
+			"location": "Chicago",
+			"yearAttended": 2013,
+			"genre": "dance music"
+		}
+	
+	]
+	
+	
+};
